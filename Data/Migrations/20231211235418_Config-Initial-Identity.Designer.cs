6f8c99247254d16f8ef1c698395918ddc2c29a27
Marcos <m.vinicius.sr11@gmail.com>
2023-12-11 20:57:28 -0300

    add migrations do Identity

--- Data/Migrations/20231211235418_Config-Initial-Identity.Designer.cs
+++ Data/Migrations/20231211235418_Config-Initial-Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SigMonografiasIfma.Data;
 
@@ -11,9 +12,11 @@ using SigMonografiasIfma.Data;
 namespace SigMonografiasIfma.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231211235418_Config-Initial-Identity")]
+    partial class ConfigInitialIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
