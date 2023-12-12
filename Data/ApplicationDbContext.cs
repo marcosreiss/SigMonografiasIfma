67a1afd0d453341ca8cf177891ec6d8a45046869
Marcos <m.vinicius.sr11@gmail.com>
2023-12-12 14:26:18 -0300

    clean solution

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using SigMonografiasIfma.Data.DataSeed;
 using SigMonografiasIfma.Models;
 
 namespace SigMonografiasIfma.Data
@@ -13,52 +12,7 @@ namespace SigMonografiasIfma.Data
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-
-            string ADMIN_ID = "02174";
-            string ROLE_ID = "341743";
-
-            //seed admin role
-            builder.Entity<Funcionario>().HasData(new Funcionario
-            {
-                Nome = "ChefeSetor",
-                NormalizedUserName = "CHEFESETOR",
-                Id = int.Parse(ROLE_ID),
-                ConcurrencyStamp = ROLE_ID
-            });
-
-            //create user
-            var appUser = new Funcionario
-            {
-                Id = int.Parse(ROLE_ID),
-                Email = "chefedesetor@email.com",
-                EmailConfirmed = true,
-                UserName = "ChefeSetor",
-                Login = "ChefeSetor",
-                Nome = "Chefe de Setor",
-                Telefone = "98989133135",
-                Cidade = "São Luís",
-                Campus = "Monte Castelo",
-                NivelAcesso = Enuns.TipoFuncionario.ChefeSetor,
-                PhoneNumber = "98989133135",
-                NormalizedUserName = "CHEFEDESETOR"
-            };
-
-            //set user password
-            PasswordHasher<Funcionario> ph = new PasswordHasher<Funcionario>();
-            appUser.PasswordHash = ph.HashPassword(appUser, "Chefesetor123!");
-
-            //seed user
-            builder.Entity<Funcionario>().HasData(appUser);
-
-            //set user role to admin
-            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
-            {
-                RoleId = ROLE_ID,
-                UserId = ADMIN_ID
-            });
-        }
+        
 
 
         public DbSet<Aluno> Alunos { get; set; }
